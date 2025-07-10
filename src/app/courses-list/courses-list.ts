914eda045d6062f1f5f8a7d1f81b8b6fce1ef2a3
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-07-10 22:28:35 +0530

    Creating the course-card component

--- src/app/courses-list/courses-list.ts
+++ src/app/courses-list/courses-list.ts
@@ -1,9 +1,9 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-courses-list',
-  imports: [CommonModule],
+  imports: [CommonModule, DatePipe, CurrencyPipe],
   templateUrl: './courses-list.html',
   styleUrl: './courses-list.css'
 })
@@ -11,10 +11,10 @@ export class CoursesList {
   title = 'Available Courses';
 
   courses = [
-    { id: 1, title: 'Angular Basics', description: 'Learn the basics of Angular framework', price: 150, duration: '4 weeks', soldOut: false, img: 'angular-icon.png' },
-    { id: 2, title: 'React Fundamentals', description: 'Understand the core concepts of React', price: 120, duration: '5 weeks', soldOut: true, img: 'react-icon.png' },
-    { id: 3, title: 'Vue.js Essentials', description: 'Get started with Vue.js', price: 40, duration: '3 weeks', soldOut: true, img: 'vue-icon.png' },
-    { id: 4, title: 'Node.js Introduction', description: 'Learn about server-side development with Node.js', price: 110, duration: '6 weeks', soldOut: false, img: 'node-icon.png' }
+    { id: 1, title: 'Angular Basics', description: 'Learn the basics of Angular framework', price: 150, startDate: '2025.08.21', duration: '4 weeks', soldOut: false, img: 'angular-icon.png', onSale: true },
+    { id: 2, title: 'React Fundamentals', description: 'Understand the core concepts of React', price: 120, startDate: '2025.10.10', duration: '5 weeks', soldOut: true, img: 'react-icon.png', onSale: false },
+    { id: 3, title: 'Vue.js Essentials', description: 'Get started with Vue.js', price: 40, startDate: '2025.11.17', duration: '3 weeks', soldOut: true, img: 'vue-icon.png', onSale: true },
+    { id: 4, title: 'Node.js Introduction', description: 'Learn about server-side development with Node.js', price: 110, startDate: '2025.12.06', duration: '6 weeks', soldOut: false, img: 'node-icon.png', onSale: false }
   ];
 
   viewDetails(course: any) {
